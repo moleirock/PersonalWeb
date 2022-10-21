c2248949581e0aab67d29f6bea7c056f013851b4
Moleiro <alberto.moleiro7@gmail.com>
2022-10-21 21:27:19 +0200

    Fixed/ fps and movement fixed in all devices

--- js/toggleTheme.js
+++
@@ -1,7 +0,0 @@
-export function swapTheme() {
-    toggleTheme.addEventListener("click", (e) => {
-        document.body.classList.toggle("light");
-        sun.classList.toggle("display-none");
-        moon.classList.toggle("display-none");
-    });
-}
