6901b29091f3f80698bcb602f31f465ba3540a3f
MoleiroClase <alberto.moleiro7@gmail.com>
2022-12-15 17:24:48 +0100

    Working in project style

--- app/js/Functions/projectFilter.js
+++ app/js/Functions/projectFilter.js
@@ -1,12 +1,16 @@
 const filterButtons = document.querySelectorAll(".filter-list__item");
-const projects = document.querySelectorAll(".project__item");
-
+const projectItems = document.querySelectorAll(".project__item");
+const projectContainer = document.querySelector(".projexts__content");
 export function projectFilter(){
     filterButtons.forEach((button) => {
-        button.addEventListener("click", () => {
+        button.addEventListener("click", (e) => {
+            filterButtons.forEach((button) => {
+                button.classList.remove("filterActive");
+            });
+            e.target.classList.add("filterActive");
             const filter = button.dataset.filter;
             console.log(filter);
-            projects.forEach((project) => {
+            projectItems.forEach((project) => {
                 if (filter === "all") {
                     project.classList.remove("display-none");
                 } else {
@@ -17,6 +21,33 @@ export function projectFilter(){
                     }
                 }
             });
+            
         });
     });
+    projectVisibility();
+    
 };
+
+
+
+//function that use intersection observer to animate the elements when they are in the element projectContainer
+function projectVisibility(){
+    const options = {
+        root: projectContainer,
+        threshold: .8
+    };
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove("darken");
+            } else {
+                entry.target.classList.add("darken");
+
+            }
+        });
+    }, options);
+
+    projectItems.forEach((project) => {
+        observer.observe(project);
+    });
+}
