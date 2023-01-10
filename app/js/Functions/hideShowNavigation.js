6067dcedbb1a1737ef707e0f91cdb916ac52880f
Moleiro <alberto.moleiro7@gmail.com>
2023-01-10 11:03:08 +0100

    FIX:bug by hideShowNavigation function fixed changing the way to hide the theme button

--- app/js/Functions/hideShowNavigation.js
+++ app/js/Functions/hideShowNavigation.js
@@ -4,11 +4,11 @@ const dropMenu = document.getElementById("dropMenu");
 
 export function hideNavigation() {
     navigationControl.style.transform = "translate(-100%)";
-    toggleTheme.style.right = "-5rem";
+    toggleTheme.classList.add("translate-right-up");
     dropMenu.style.transform = "translate(0)";
 }
 export function showNavigation() {
     navigationControl.style.transform = "unset";
-    toggleTheme.style.right = "1rem";
+    toggleTheme.classList.remove("translate-right-up");
     dropMenu.style.transform = "translate(100%,-100%)";
 }
